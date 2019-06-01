95dd61c29758a73ec3f5039fd69d1fbfabaf2ec5
Winterroll <winterrollxp@gmail.com>
2019-06-01 13:23:00 -0400

    Isolated component to other file.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, {Fragment,useState,useEffect} from 'react';
 import {Dropdown} from "semantic-ui-react";
 import './App.css';
+import DevidedDropDown from "./components/devidedDropdown";
 
 
 function App() {
   const masterData={
-    active:[{
+    Active:[{
       name:"test name 1",
       id:0,
     },{
@@ -15,7 +16,7 @@ function App() {
       name:"test name 3",
       id:2,
     }],
-    other:[{
+    Other:[{
       name:"test name 4",
       id:3,
     },{
@@ -43,49 +44,5 @@ function App() {
   );
 }
 
-function DevidedDropDown(props) {
-  const masterData = props.masterData || [];
-  const options = getDropdownOptions(masterData);
-
-  function getDropdownOptions(masterData) {
-    const result = [];
-    let keyCount = 0;
-    const keys = Object.keys(masterData);
-    if(keys.length > 0){
-      keys.forEach((property,index)=>{
-        if(masterData[property] && masterData[property].length > 0){
-          result.push(
-            { text:property,
-              key : keyCount++,
-              value : -1,
-              disabled : true,
-            }
-          );
-          masterData[property].forEach((item,index)=>{
-            let temp = {};
-            temp.text = item.name;
-            temp.value = item.id;
-            temp.key = keyCount ++;
-            result.push(temp);
-          }); 
-        }
-      });
-    }
-    
-    return result;
-  }
-
-  return (
-    <Dropdown
-    {...props}
-    placeholder={props.placeholder}
-    fluid
-    selection
-    options={options}
-    onChange = {props.onChange}
-    />
-  );
-  
-}
 
 export default App;
